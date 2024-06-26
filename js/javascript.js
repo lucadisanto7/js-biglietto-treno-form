883ffa9da003fe95d1bde5ad6575d73adb6037e9
lucadisanto7 <lucadisanto47@gmail.com>
2024-06-26 13:41:36 +0200

    [UPD] Added inputs in HTML and js code

---
+++ js/javascript.js
@@ -0,0 +1,24 @@
+let age = prompt('inserisci la tua eta');
+let chilometri = prompt('inserisci i chilometri da percorrere');
+
+let prezzo_biglietto = 0.21 * chilometri;
+let sconto = 0
+let prezzo_scontato
+let prezzo_finale
+
+if (age < 18){
+    sconto = 20;
+}
+else if (age > 65){
+    sconto = 40;
+}
+if (sconto != 0){
+    prezzo_scontato = prezzo_biglietto * sconto / 100;
+    prezzo_finale = prezzo_biglietto - prezzo_scontato
+    console.log(prezzo_scontato)
+}
+else{
+    prezzo_finale = prezzo_biglietto
+}
+
+console.log(prezzo_finale)
